34eeee866f7ac65aee5856da1a539a02b72fe602
AnThuyNguyen <nguyen.ann.1411@gmail.com>
2026-04-24 04:12:18 -0700

    Routing around the pages

--- frontend/src/features/onboarding/pages/InputPage.jsx
+++ frontend/src/features/onboarding/pages/InputPage.jsx
@@ -1,8 +1,12 @@
 import { theme } from "../../../styles/theme";
+import { useNavigate } from "react-router-dom";
 import HeaderBar from "../../../components/ui/HeaderBar";
 import ProgressBar from "../../../components/ui/ProgressBar";
+import AppButton from "../../../components/ui/AppButton";
 
 export default function InputPage({ setShowInputPage }) {
+    const navigate = useNavigate();
+
   return (
     <main
     style={{
@@ -17,6 +21,11 @@ export default function InputPage({ setShowInputPage }) {
         <HeaderBar leftButton={"Change fit ←"} > 
             <ProgressBar/>
         </HeaderBar>
+        <AppButton               
+        onClick={() => navigate("/occasion")}
+        >
+             Continue 
+    </AppButton>
     </main>
     );
 }
