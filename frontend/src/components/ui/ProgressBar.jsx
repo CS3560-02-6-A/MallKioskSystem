98f9eb95fc3277ef2b2ea767e9836ae16366ffea
AnThuyNguyen <nguyen.ann.1411@gmail.com>
2026-04-23 19:14:30 -0700

    Implemented with no connection yet

---
+++ frontend/src/components/ui/ProgressBar.jsx
@@ -0,0 +1,23 @@
+import { theme } from "../../styles/theme";
+
+export default function ProgressBar(step = 1, totalSteps = 3) {
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "clamp(25px, 2.5vw, 30px)",
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr 1fr",
+        gap: "6px",
+        borderRadius: theme.radius.pill,
+        overflow: "hidden",
+        padding: "6px",
+      }}
+    >
+      <div style={{ background: theme.colors.eggplant, borderRadius: "999px 0 0 999px", boxShadow: theme.shadows.dropShadow}} />
+      <div style={{ background: step > 1 ? theme.colors.eggplant : theme.colors.palePink, boxShadow: theme.shadows.dropShadow }} />
+      <div style={{ background: step > 2 ? theme.colors.eggplant : theme.colors.palePink, borderRadius: "0 999px 999px 0", boxShadow: theme.shadows.dropShadow }} />
+    </div>
+  );
+}
