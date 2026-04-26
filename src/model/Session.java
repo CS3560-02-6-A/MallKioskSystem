a25ba7214f78b16ad39c91d980400c5d10a741dc
BrookeWork <landry@cpp.edu>
2026-04-25 21:44:26 -0700

    Add files via upload

---
+++ src/model/Session.java
@@ -0,0 +1,26 @@
+package src.model;
+import java.time.LocalDateTime;
+public class Session {
+	private int sessionID;
+	private LocalDateTime startTime;
+	private LocalDateTime endTime;
+	public Session(int sessionID, LocalDateTime startTime, LocalDateTime endTime) {
+		setSessionID(sessionID);
+		setStartTime(startTime);
+		setEndTime(endTime);
+	}
+	public Session(int sessionID, boolean startSession) {
+		setSessionID(sessionID);
+		if (startSession) {
+			startTime = LocalDateTime.now();
+		}
+	}
+	public Session(int sessionID) {setSessionID(sessionID);}
+	public void endSession() {endTime = LocalDateTime.now();}
+	public void setSessionID(int sessionID) {this.sessionID = sessionID;}
+	public void setStartTime(LocalDateTime startTime) {this.startTime = startTime;}
+	public void setEndTime(LocalDateTime endTime) {this.endTime = endTime;}
+	public int getSessionID() {return sessionID;}
+	public LocalDateTime getStartTime() {return startTime;}
+	public LocalDateTime getEndTime() {return endTime;}
+}
--- src/model/StoreHours.java
+++ src/model/StoreHours.java
@@ -1,40 +1,33 @@
 package src.model;
-public class StoreHours 
-{
+public class StoreHours {
 	private int hoursID;
 	private int storeID;
 	private String dayOfWeek;
 	private Time openTime;
 	private Time closeTime;
-
-	public StoreHours(int hoursID, int storeID, String dayOfWeek, Time openTime, Time closeTime) 
-	{
-		this.hoursID = hoursID;
-		this.storeID = storeID;
-		this.dayOfWeek = dayOfWeek;
-		this.openTime = openTime;
-		this.closeTime = closeTime;
+	public StoreHours(int hoursID, int storeID, String dayOfWeek, Time openTime, Time closeTime) {
+		setHoursID(hoursID);
+		setStoreID(storeID);
+		setDayOfWeek(dayOfWeek);
+		setOpenTime(openTime);
+		setCloseTime(closeTime);
 	}
-	public void setHoursID(int hoursID) 
-	{
+	public void setHoursID(int hoursID) {
 		//We must make sure that this hours ID is not the same
 		//as any other hours ID connected to a particular store.
 		//Where may I find a function to check for this?
 		this.hoursID = hoursID;
 	}
-	public void setStoreID(int storeID) 
-	{
+	public void setStoreID(int storeID) {
 		//We must make sure that this store ID is not the same
 		//as any other store ID. Where may I find a function to
 		//check for this?
 		this.storeID = storeID;
 	}
-	public void setDayOfWeek(String dayOfWeek) 
-	{
+	public void setDayOfWeek(String dayOfWeek) {
 		//Store can make sure that it does not have multiple
 		//operation hours on the same day of week.
-		switch (dayOfWeek.toLowerCase()) 
-		{
+		switch (dayOfWeek.toLowerCase()) {
 			case "monday":
 			case "tuesday":
 			case "wednesday":
@@ -43,37 +36,29 @@ public class StoreHours
 			case "saturday":
 			case "sunday":
 				this.dayOfWeek = dayOfWeek.toLowerCase();
-			break;
 			default:
 				throw new IllegalArgumentException("Not a real day of the week.");
 		}
 	}
-	public void setOpenTime(Time openTime) 
-	{
+	public void setOpenTime(Time openTime) {
 		this.openTime = openTime;
 	}
-	public void setCloseTime(Time closeTime) 
-	{
+	public void setCloseTime(Time closeTime) {
 		this.closeTime = closeTime;
 	}
-	public int getHoursID() 
-	{
+	public int getHoursID() {
 		return hoursID;
 	}
-	public int getStoreID() 
-	{
+	public int getStoreID() {
 		return storeID;
 	}
-	public String getDayOfWeek() 
-	{
+	public String getDayOfWeek() {
 		return dayOfWeek;
 	}
-	public Time getOpenTime() 
-	{
-		return new Time(openTime.getHour(), openTime.getMinute());
+	public Time getOpenTime() {
+		return new Time(openTime.hour, openTime.minute);
 	}
-	public Time getCloseTime() 
-	{
-		return new Time(closeTime.getHour(), closeTime.getMinute());
+	public Time getCloseTime() {
+		return new Time(closeTime.hour, closeTime.minute);
 	}
 }
