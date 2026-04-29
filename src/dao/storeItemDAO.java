ac8c08dfdcc7440e46abc629e5a54fc3a1d65cae
Anthony Corona <anthonycorona@Anthonys-MacBook-Pro.local>
2026-04-29 13:54:22 -0700

    added full data item and filtered query

--- src/dao/storeItemDAO.java
+++ src/dao/storeItemDAO.java
@@ -79,4 +79,89 @@ public class storeItemDAO {
 
         return items;
     }
+
+    public List<StoreItem> getItemsFullData() {
+    List<StoreItem> items = new ArrayList<>();
+
+    String sql = "SELECT si.storeID, si.itemID, si.inStock, si.price, si.aisle, " +
+                 "i.name, i.type, i.color, i.gender, i.occasion " +
+                 "FROM inventory_tbl si " +
+                 "JOIN items_tbl i ON si.itemID = i.ID" + 
+                 "WHERE si.inStock = true"
+                 ;
+
+    try (Connection conn = databaseConnection.getConnection();
+         PreparedStatement ps = conn.prepareStatement(sql);
+         ResultSet rs = ps.executeQuery()) {
+
+        while (rs.next()) {
+            StoreItem item = new StoreItem(
+                rs.getInt("storeID"),
+                rs.getInt("itemID"),
+                rs.getBoolean("inStock"),
+                rs.getDouble("price"),
+                rs.getString("aisle"),
+                rs.getString("name"),
+                rs.getString("type"),
+                rs.getString("color"),
+                rs.getString("gender"),
+                rs.getString("occasion")
+            );
+            items.add(item);
+        }
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+
+    return items;
+}
+
+ public List<StoreItem> getItemsFullDataWithFilters(String gender, String occasion) {
+    List<StoreItem> items = new ArrayList<>();
+
+    String sql = "SELECT si.storeID, si.itemID, si.inStock, si.price, si.aisle, " +
+                 "i.name, i.type, i.color, i.gender, i.occasion " +
+                 "FROM inventory_tbl si " +
+                 "JOIN items_tbl i ON si.itemID = i.ID " +  
+                 "WHERE si.inStock = true " +
+                 "AND (i.gender = ? OR i.gender = 'unisex' OR ? IS NULL) " +
+                 "AND (i.occasion = ? OR ? IS NULL)";
+
+    try (Connection conn = databaseConnection.getConnection();
+         PreparedStatement ps = conn.prepareStatement(sql)) {
+
+        // Gender (used twice)
+        ps.setString(1, gender);
+        ps.setString(2, gender);
+
+        // Occasion (used twice)
+        ps.setString(3, occasion);
+        ps.setString(4, occasion);
+
+        try (ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                StoreItem item = new StoreItem(
+                    rs.getInt("storeID"),
+                    rs.getInt("itemID"),
+                    rs.getBoolean("inStock"),
+                    rs.getDouble("price"),
+                    rs.getString("aisle"),
+                    rs.getString("name"),
+                    rs.getString("type"),
+                    rs.getString("color"),
+                    rs.getString("gender"),
+                    rs.getString("occasion")
+                );
+                items.add(item);
+            }
+        }
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+
+    return items;
+}
+
 }
