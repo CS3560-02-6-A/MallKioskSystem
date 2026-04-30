61215138fe829b85a0c4630bd668baa649130d3c
BrookeWork <landry@cpp.edu>
2026-04-29 22:56:47 -0700

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
+import ReceiptPage from "./features/outfit-curator/pages/ReceiptPage";
 import OnboardingPage from "./features/onboarding/pages/OnboardingPage";
 import InputPage from "./features/onboarding/pages/InputPage";
 import OccasionPage from "./features/outfit-curator/pages/OccasionPage";
+import OutfitBuilderPage from "./features/outfit-curator/pages/OutfitBuilderPage";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<OnboardingPage />} /> {/* Default route */}
+      <Route path="*" element={<Navigate to="/onboarding" replace />} />
       <Route path="/onboarding" element={<OnboardingPage />} />
       <Route path="/input" element={<InputPage />} />
       <Route path="/occasion" element={<OccasionPage />} />
-      <Route path="*" element={<Navigate to="/onboarding" replace />} />
-
-      {/* <Route path="/gender" element={<GenderPage />} />
-      
-      <Route path="/outfit" element={<OutfitBuilderPage />} /> */}
+      <Route path="/outfit" element={<OutfitBuilderPage />} />
+      <Route path="/receipt" element={<ReceiptPage />} />
     </Routes>
   );
 }
