d60654a486a8a207943ea9d21648aa9a846a0eb2
Chau <chau.nguyen.dev@gmail.com>
2026-04-29 23:33:55 -0700

    Fix StoreHours.java constructor

--- src/model/StoreHours.java
+++ src/model/StoreHours.java
@@ -6,30 +6,36 @@ public class StoreHours {
 	private Time openTime;
 	private Time closeTime;
 
-	public StoreHours(int hoursID, int storeID) {
-		setHoursID(hoursID);
-		setStoreID(storeID);
+	public StoreHours(int hoursID, int storeID) 
+	{
+		this.hoursID = hoursID;
+		this.storeID = storeID;
 	}
-	public StoreHours(int hoursID, int storeID, String dayOfWeek, Time openTime, Time closeTime) {
-		setHoursID(hoursID);
-		setStoreID(storeID);
-		setDayOfWeek(dayOfWeek);
-		setOpenTime(openTime);
-		setCloseTime(closeTime);
+	public StoreHours(int hoursID, int storeID, String dayOfWeek, Time openTime, Time closeTime) 
+	{
+		this.hoursID = hoursID;
+		this.storeID = storeID;
+		this.dayOfWeek = dayOfWeek;
+		this.openTime = openTime;
+		this.closeTime = closeTime;
+
 	}
-	public void setHoursID(int hoursID) {
+	public void setHoursID(int hoursID) 
+	{
 		//We must make sure that this hours ID is not the same
 		//as any other hours ID connected to a particular store.
 		//Where may I find a function to check for this?
 		this.hoursID = hoursID;
 	}
-	public void setStoreID(int storeID) {
+	public void setStoreID(int storeID) 
+	{
 		//We must make sure that this store ID is not the same
 		//as any other store ID. Where may I find a function to
 		//check for this?
 		this.storeID = storeID;
 	}
-	public void setDayOfWeek(String dayOfWeek) {
+	public void setDayOfWeek(String dayOfWeek) 
+	{
 		//Store can make sure that it does not have multiple
 		//operation hours on the same day of week.
 		switch (dayOfWeek.toLowerCase()) {
@@ -46,7 +52,8 @@ public class StoreHours {
 				throw new IllegalArgumentException("Not a real day of the week.");
 		}
 	}
-	public void setOpenTime(Time openTime) {
+	public void setOpenTime(Time openTime) 
+	{
 		this.openTime = openTime;
 	}
 	public void setCloseTime(Time closeTime) {
