8818089c79946f4aae02d727d2090e078311f03b
Chau <chau.nguyen.dev@gmail.com>
2026-04-29 23:40:33 -0700

    Add setter functions for StoreItem.java class

--- src/model/StoreItem.java
+++ src/model/StoreItem.java
@@ -1,4 +1,6 @@
 package src.model;
+import java.sql.*;
+imprt java.util.*;
 
 public class StoreItem 
 {
@@ -38,6 +40,7 @@ public class StoreItem
         this.occasion = occasion;
     }
 
+    // Getters
     public int getStoreId() { return storeId; }
     public int getItemId() { return itemId; }
     public boolean isInStock() { return inStock; }
@@ -49,4 +52,18 @@ public class StoreItem
     public String getItemColor() { return color; }
     public String getGender() { return gender; }
     public String getOccasion() { return occasion; }
+
+    // Setters
+    public void setStoreId(int storeId) { this.storeId = storeId; }
+    public void setItemId(int itemId) { this.itemId = itemId; }
+    public void setInStock(boolean inStock) { this.inStock = inStock; }
+    public void setPrice(double price) { this.price = price; }
+    public void setAisle(String aisle) { this.aisle = aisle; }
+    public void setItemName(String name) { this.name = name; }
+    public void setItemType(String type) { this.type = type; }
+    public void setItemColor(String color) { this.color = color; }
+    public void setGender(String gender) { this.gender = gender; }
+    public void setOccasion(String occasion) { this.occasion = occasion; }
+
+    
 }
