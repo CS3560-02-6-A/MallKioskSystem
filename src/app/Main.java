72cd1af2bdea56c596dc177172d9c8e3c576e44b
BrookeWork <landry@cpp.edu>
2026-05-01 14:03:26 -0700

    Add files via upload

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,30 +1,28 @@
 package src.app;
 
 import java.util.List;
-import src.dao.itemDAO;
 import src.dao.storeItemDAO;
+import src.model.Receipt;
 import src.model.StoreItem;
-import src.model.Item;
+import src.model.outfitGenerator;
+import src.service.mallKioskService;
 
 public class Main 
 {
     public static void main(String[] args) 
     {
-        System.out.println(
-            "\n\n---------------BEGIN MAIN TESTING------------");
+        System.out.println("BEGIN MAIN TESTING");
 
-
-
-
+        mallKioskService service = new mallKioskService();
+        outfitGenerator generator = new outfitGenerator();
         storeItemDAO dao = new storeItemDAO();
-            //Testing for getting all items with full data paramaters, no filtering
-            List<StoreItem> items = dao.getItemsFullData();
-            //Testing for getting all items with full data paramaters, filtered
-            List<StoreItem> filteredItems = dao.getItemsFullDataWithFilters("women", "casual");
 
-            for (StoreItem i : filteredItems) {
-        System.out.println(
-        "Name: " + i.getItemName() +
+        List<StoreItem> outfit = service.generateOutfit("women", null);
+        
+          for (StoreItem i : outfit)
+        {
+            System.out.println(
+               "Name: " + i.getItemName() +
             " | Type: " + i.getItemType() +
             " | Color: " + i.getItemColor() +
             " | Gender: " + i.getGender() +
@@ -35,45 +33,89 @@ public class Main
             " | InStock: " + i.isInStock() +
             " | Aisle: " + i.getAisle()
             );
-    //          public int getStoreId() { return storeId; }
-    // public int getItemId() { return itemId; }
-    // public boolean isInStock() { return inStock; }
-    // public double getPrice() { return price; }
-    // public String getAisle() { return aisle; }
-    // public int getItemID() { return itemId; }
-    // public String getItemName() { return name; }
-    // public String getItemType() { return type; }
-    // public String getItemColor() { return color; }
-    // public String getGender() { return gender; }
-    // public String getOccasion() { return occasion; }
         }
 
+        Receipt receipt = service.generaReceipt(outfit, 1);
+        System.out.println("Receipt total: $" + receipt.getTotalPrice() + " | Generated at: " + receipt.getGeneratedAt());
 
 
-        // List<StoreItem> items = dao.getAllItems();
-            //TESTING FOR getItemsByGenderAndOccasion
-            //Need to verify to use StoreItem or Item
-        // List<Item> filteredItems = dao.getItemsByGenderAndOccasion("woman", "casual");
 
-        // for(Item i : filteredItems)
+
+        // System.out.println("TESTING storeItemDAO...");
+        // storeItemDAO dao = new storeItemDAO();
+        
+
+        //Testing for getting all items with full data paramaters, filtered.
+        // System.out.println("Testing for getting all items with full data paramaters, filtered.");
+        // List<StoreItem> filteredItems = dao.getItemsFullDataWithFilters("men", null);
+      
+        System.out.println("Testing for getting all items with full data paramaters, filtered.");
+        List<StoreItem> filteredItems = dao.getItemsFullDataWithFilters("women", "formal");
+
+        // for (StoreItem i : filteredItems) 
         // {
-        //     System.out.println(i.getItemName() + " - " + i.getGender() + " - " + i.getOccasion());
+        //     System.out.println(
+        //        "Name: " + i.getItemName() +
+        //     " | Type: " + i.getItemType() +
+        //     " | Color: " + i.getItemColor() +
+        //     " | Gender: " + i.getGender() +
+        //     " | Occasion: " + i.getOccasion() +
+        //     " | StoreID: " + i.getStoreId() +
+        //     " | ItemID: " + i.getItemId() +
+        //     " | Price: $" + i.getPrice() +
+        //     " | InStock: " + i.isInStock() +
+        //     " | Aisle: " + i.getAisle()
+        //     );
         // }
 
+    //  try (Connection conn = databaseConnection.getConnection();
+    //          Statement s = conn.createStatement();
+    //          ResultSet rs = s.executeQuery("SELECT DATABASE(), @@hostname, @@port")) {
 
-        // for (StoreItem inItem : items) {
-        //     System.out.println(inItem.getItemId());
+    //         System.out.println("URL: " + conn.getMetaData().getURL());
+
+    //         while (rs.next()) {
+    //             System.out.println("Database: " + rs.getString(1));
+    //             System.out.println("Host: " + rs.getString(2));
+    //             System.out.println("Port: " + rs.getInt(3));
+    //         }
+
+    //     } catch (Exception e) {
+    //         e.printStackTrace();
+    //     }
+
+
+        // Testing for getting just accessories with full data paramaters, filtered.
+        // System.out.println("Testing for getting just accessories with full data paramaters, filtered.");
+        // outfitGenerator generator = new outfitGenerator();
+        // List<StoreItem> outfit = generator.generateFullOutfitList(filteredItems);
+        
+        //   for (StoreItem i : outfit)
+        // {
+        //     System.out.println(
+        //        "Name: " + i.getItemName() +
+        //     " | Type: " + i.getItemType() +
+        //     " | Color: " + i.getItemColor() +
+        //     " | Gender: " + i.getGender() +
+        //     " | Occasion: " + i.getOccasion() +
+        //     " | StoreID: " + i.getStoreId() +
+        //     " | ItemID: " + i.getItemId() +
+        //     " | Price: $" + i.getPrice() +
+        //     " | InStock: " + i.isInStock() +
+        //     " | Aisle: " + i.getAisle()
+        //     );
         // }
 
         //TESTING FOR storeDAO
+        // System.out.println("TESTING FOR storeDAO...");
         // storeDAO myStoreDAO = new storeDAO();
+        //Printing out all stores.
         // List<Store> stores = myStoreDAO.getAllStores();
         // for(Store s: stores)
         // {
         //     System.out.println(s.getStoreID() + " - " + s.getStoreName());
         //     System.out.println(s.getHours().printOperationHour());
         //     System.out.println();
-
         // }
 
     }
